cda262490cf951aa405f56284f9a8b490dba9078
ozakiryota <kb22056@gmail.com>
2020-10-17 15:00:05 +0900

    updated

--- xsens_driver.sh
+++ xsens_driver.sh
@@ -5,4 +5,4 @@ image_name="xsens_driver"
 docker run -it --rm \
 	--net=host \
 	$image_name:latest \
-	bash -c "source /home/ros_catkin_ws/devel/setup.bash;roslaunch xsens_driver xsens_driver.launch"
+	bash -c "source /home/ros_catkin_ws/devel/setup.bash;roslaunch xsens_driver xsens.launch"
